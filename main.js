17cb61dd3846ae141a241eb0c59734b05fb79e54
Mikeal Rogers <mikeal.rogers@gmail.com>
2011-03-04 18:05:15 -0800

    Initial commit.

---
+++ main.js
@@ -0,0 +1,40 @@
+var stream = require('stream')
+  , fs = require('fs')
+  , util = require('util')
+  ;
+
+BufferedStream = function (limit) {
+  if (typeof limit === 'undefined') {
+    limit = Infinity;
+  }
+  this.limit = limit;
+  this.size = 0;
+  this.chunks = [];
+  this.writable = true;
+  this.readable = true;
+}
+util.inherits(BufferedStream, stream.Stream);
+BufferedStream.prototype.pipe = function () {
+  this.dest = dest = arguments[0];
+  if (this.resume) this.resume();
+  stream.Stream.prototype.pipe.apply(this, arguments);
+  this.chunks.forEach(function (c) {dest.write(c)})
+  this.size = 0;
+  delete this.chunks;
+}
+BufferedStream.prototype.write = function (chunk) {
+  if (this.dest) {
+    this.emit('data', chunk);
+    return;
+  }
+  this.chunks.push(chunk);
+  this.size += chunk.length;
+  if (this.limit < this.size) {
+    this.pause();
+  }
+}
+BufferedStream.prototype.end = function () {
+  this.emit('end');
+}
+
+exports.BufferedStream = BufferedStream;
