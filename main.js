03e9ea31a65df66a9c3cf95c8cbf1fe51f27b5e4
Marco Rogers <marco.rogers@gmail.com>
2011-03-06 15:15:36 -0500

    add bufferSize method to hide buffering implementation

--- main.js
+++ main.js
@@ -14,13 +14,16 @@ BufferedStream = function (limit) {
   this.readable = true;
 }
 util.inherits(BufferedStream, stream.Stream);
+BufferedStream.prototype.bufferSize = function() {
+  return this.chunks.length;
+}
 BufferedStream.prototype.pipe = function () {
   var dest = this.dest = arguments[0];
   if (this.resume) this.resume();
   stream.Stream.prototype.pipe.apply(this, arguments);
   this.chunks.forEach(function (c) {dest.write(c)})
   this.size = 0;
-  delete this.chunks;
+  this.chunks = [];
 }
 BufferedStream.prototype.write = function (chunk) {
   if (this.dest) {
